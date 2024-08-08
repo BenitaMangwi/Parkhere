f845a21b34644e2c7b0090e1114e17641e60a4c6
BenitaMangwi <mangwibenita10@gmail.com>
2024-08-08 22:32:32 +0100

    setup page and view

--- app/public/scripts/map.js
+++ app/public/scripts/map.js
@@ -3,6 +3,7 @@ function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 51.5074, lng: 0.1278 },
     zoom: 12,
+    key: "AIzaSyCkPjRJiiqhUZFJ4T_OJULzHnxlVXHzxEo",
   });
 
   // Fetch parking data from your API or manually create an array of parking objects
