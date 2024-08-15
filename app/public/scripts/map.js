b954fb3e2b1f06b34bd8f2bc10627ee8d4fdbb84
BenitaMangwi <mangwibenita10@gmail.com>
2024-08-15 20:01:43 +0100

    setup and view

--- app/public/scripts/map.js
+++ app/public/scripts/map.js
@@ -1,6 +1,4 @@
 
-
-
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 51.5074, lng: 0.1278 },
@@ -9,7 +7,7 @@ function initMap() {
 
   // Fetch parking data from your API or manually create an array of parking objects
   const parkings = [
-    // ... parking data
+    
   ];
 
   parkings.forEach((parking) => {
